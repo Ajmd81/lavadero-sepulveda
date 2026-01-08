c0715d4b8d3db44cc757be21ae79ddeee260af10
Ajmd81 <martinezdiazantoniojesus@gmail.com>
2026-01-08 18:36:07 +0100

    App: Añadido el import que faltaba

--- src/main/java/com/lavaderosepulveda/app/service/CitaService.java
+++ src/main/java/com/lavaderosepulveda/app/service/CitaService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.DayOfWeek;
 import java.util.*;
 import java.util.stream.Collectors;
 
