99d6bd11df64f76f16aa54896e378d6774da0e48
Ajmd81 <martinezdiazantoniojesus@gmail.com>
2026-01-25 18:37:12 +0100

    fix: Cambiar allowedOrigins por allowedOriginPatterns en CORS

--- src/main/java/com/lavaderosepulveda/app/config/SecurityConfig.java
+++ src/main/java/com/lavaderosepulveda/app/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         // Orígenes permitidos
-        configuration.setAllowedOrigins(Arrays.asList(
+        configuration.setAllowedOriginsPatterns(Arrays.asList(
                 "https://lavadero-sepulveda-production.up.railway.app",
                 "http://localhost:8080",
                 "http://127.0.0.1:8080",
